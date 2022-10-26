06be1dbaf40c428ae3b33a8d01eda062670a4c69
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-10-26 19:49:18 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-      .setTitle('Thoi khoa bieu')
-      .setDescription('Thoi khoa bieu API description')
-      .setVersion('1.0')
-      .addTag('tkb')
-      .build();
+    .setTitle('Thoi khoa bieu')
+    .setDescription('Thoi khoa bieu API description')
+    .setVersion('1.0')
+    .addTag('tkb')
+    .build();
 
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
