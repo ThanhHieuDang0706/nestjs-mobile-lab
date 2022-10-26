a23b571aae7f6ef1cbb69051bbaeba0fcba644e7
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-10-26 20:16:26 +0700

    add get tkb by id

--- src/thoi-khoa-bieu/thoi-khoa-bieu.controller.ts
+++ src/thoi-khoa-bieu/thoi-khoa-bieu.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { thoiKhoaBieu } from './thoi-khoa-bieu.interface';
 import * as thoiKhoaBieuData from './thoiKhoaBieu.json';
 
@@ -7,5 +7,19 @@ export class ThoiKhoaBieuController {
   @Get()
   findAll(): thoiKhoaBieu[] {
     return thoiKhoaBieuData.thoiKhoaBieu;
+  }
+
+  @Get(':id')
+  findOne(@Param() params): thoiKhoaBieu {
+    return thoiKhoaBieuData.thoiKhoaBieu.find(
+      (tkb) => tkb.id === Number(params.id),
+    );
+  }
+
+  @Get('sinh-vien/:id')
+  findTKBBySinhVien(@Param() params): thoiKhoaBieu[] {
+    return thoiKhoaBieuData.thoiKhoaBieu.filter(
+      (tkb) => tkb.id_sinh_vien === Number(params.id),
+    );
   }
 }
